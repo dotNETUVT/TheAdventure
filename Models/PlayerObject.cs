afb8d0d43210af34685b172f9058db00c6d41778
DeniBD <denisa.buftea02@e-uvt.ro>
2024-04-07 01:26:46 +0300

    Added animation for player movement

--- Models/PlayerObject.cs
+++ Models/PlayerObject.cs
@@ -20,9 +20,29 @@ public class PlayerObject : GameObject
     private int _textureId;
     private int _pixelsPerSecond = 128;
 
+    private int _frameIndex;
+    private double _animationTime;
+    private const double FrameDuration = 0.1;
+    private const int FrameCount = 6;
+
+    private int FrameWidth;
+    private int FrameHeight;
+    
+    private readonly int RowForDown = 3;
+    private readonly int RowForRight = 4;
+    private readonly int RowForUp = 5; 
+    private readonly int RowForLeft = 6;
+
     public PlayerObject(int id) : base(id)
     {
         _textureId = GameRenderer.LoadTexture(Path.Combine("Assets", "player.png"), out var textureData);
+        UpdateScreenTarget();
+        
+        FrameWidth = textureData.Width / FrameCount;
+        FrameHeight = textureData.Height / 11; // 11 rows in the sprite sheet because I added a row for the player moving left.
+        _frameIndex = 0;
+        _animationTime = 0;
+        
         UpdateScreenTarget();
     }
 
@@ -42,10 +62,46 @@ public class PlayerObject : GameObject
         Y -= (int)(up * pixelsToMove);
         Y += (int)(down * pixelsToMove);
 
+        int animationRow = 0;
+        if (up > 0)
+        {
+            animationRow = RowForUp;
+        }
+        else if (down > 0)
+        {
+            animationRow = RowForDown;
+        }
+        else if (left > 0)
+        {
+            animationRow = RowForLeft;
+        }
+        else if (right > 0)
+        {
+            animationRow = RowForRight;
+        }
+        
+        if(up > 0 || down > 0 || left > 0 || right > 0){
+            UpdateAnimation(animationRow, time);
+        }
+
         UpdateScreenTarget();
+    }
+    
+    private void UpdateAnimation(int row, int timeSinceLastUpdate){
+        _animationTime += timeSinceLastUpdate / 1000.0;
+        if(_animationTime >= FrameDuration){
+            _frameIndex++;
+            if(_frameIndex >= FrameCount){
+                _frameIndex = 0;
+            }
+            _animationTime = 0;
+        }
+
+        _source = new Rectangle<int>(_frameIndex * FrameWidth, row * FrameHeight, FrameWidth, FrameHeight);
     }
 
     public void Render(GameRenderer renderer){
         renderer.RenderTexture(_textureId, _source, _target);
     }
+    
 }
