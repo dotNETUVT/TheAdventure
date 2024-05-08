76bfba1ad168b0659b3aaecc688b7c69a25cfbdc
Stefan30 <secrieru23022@gmail.com>
2024-05-08 17:13:42 +0300

    Safety Commit

--- Program.cs
+++ Program.cs
@@ -1,14 +1,57 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using Silk.NET.SDL;
 
 namespace TheAdventure;
 
 public static class Program
 {
+    public static void LoadPlugins(Sdl sdl)
+    {
+        LoadQuestalia("C:/Users/Stefan/RiderProjects/TheAdventure/Questalia/bin/Debug/net8.0/Questalia.dll", sdl);
+    }
+
+    public static void LoadQuestalia(string path, Sdl sdl)
+    {
+        var assembly = Assembly.LoadFrom(path);
+        Type? plugin = null;
+        
+        foreach (var type in assembly.GetTypes())
+        {
+            Console.WriteLine(type);
+            if (type.FullName == "Questalia.Questalia")
+            {
+                plugin = type;
+                break;
+            }
+
+            if (plugin == null)
+            {
+                Console.WriteLine("Could not load Questalia plugin! Aborting...");
+                return;
+            }
+        }
+
+        var constructor = plugin.GetConstructor(Type.EmptyTypes);
+
+        if (constructor == null)
+        {
+            Console.WriteLine("Constructor missing! Aborting...");
+            return;
+        }
+        var pluginInstance = constructor.Invoke(null);
+        Console.WriteLine(pluginInstance);
+        
+        var init = plugin.GetMethod("Init");
+        init.Invoke(pluginInstance, new object[] {sdl});
+        
+        Console.WriteLine("Questalia loaded!");
+        Console.WriteLine(constructor);
+    }
     public static void Main()
     {
         var sdl = new Sdl(new SdlContext());
-
+        LoadPlugins(sdl);
         var sdlInitResult = sdl.Init(Sdl.InitVideo | Sdl.InitEvents | Sdl.InitTimer | Sdl.InitGamecontroller |
                                      Sdl.InitJoystick);
         if (sdlInitResult < 0)
@@ -34,7 +77,7 @@ public static class Program
                 engine.RenderFrame();
             }
         }
-
+        
         sdl.Quit();
     }
 }
