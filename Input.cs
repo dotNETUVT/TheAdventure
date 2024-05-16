0edbe8f8377fe31186fe8a9835b1b4363a3898e0
Vaxter29 <156250261+Vaxter29@users.noreply.github.com>
2024-05-16 19:24:20 +0300

    Update Input.cs

--- Input.cs
+++ Input.cs
@@ -7,11 +7,12 @@ namespace TheAdventure
         private Sdl _sdl;
         private GameWindow _gameWindow;
         private GameRenderer _renderer;
-        
+        private bool bombKeyPressed = false;
+
         byte[] _mouseButtonStates = new byte[(int)MouseButton.Count];
-        
+
         public EventHandler<(int x, int y)> OnMouseClick;
-        
+
         public Input(Sdl sdl, GameWindow window, GameRenderer renderer)
         {
             _sdl = sdl;
@@ -19,14 +20,31 @@ namespace TheAdventure
             _renderer = renderer;
         }
 
-        public bool IsKeyAPressed(){
+        public bool IsKeyAPressed()
+        {
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.A] == 1;
         }
 
-        public bool IsKeyBPressed(){
+        public bool IsKeyBPressed()
+        {
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.B] == 1;
+        }
+
+        public bool CanDropBomb()
+        {
+            if (IsKeyBPressed())
+            {
+                if (!bombKeyPressed)
+                {
+                    bombKeyPressed = true;
+                    return true;
+                }
+                return false;
+            }
+            bombKeyPressed = false;
+            return false;
         }
 
         public bool IsLeftPressed()
@@ -34,25 +52,31 @@ namespace TheAdventure
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.Left] == 1;
         }
-        
+
         public bool IsRightPressed()
         {
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.Right] == 1;
         }
-        
+
         public bool IsUpPressed()
         {
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.Up] == 1;
         }
-        
+
         public bool IsDownPressed()
         {
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.Down] == 1;
         }
-        
+
+        public bool IsSpacePressed()
+        {
+            ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
+            return _keyboardState[(int)KeyCode.Space] == 1;
+        }
+
         public bool ProcessInput()
         {
             var currentTime = DateTimeOffset.UtcNow;
@@ -147,12 +171,12 @@ namespace TheAdventure
                         mouseX = ev.Motion.X;
                         mouseY = ev.Motion.Y;
                         _mouseButtonStates[ev.Button.Button] = 1;
-                        
+
                         if (ev.Button.Button == (byte)MouseButton.Primary)
                         {
                             OnMouseClick?.Invoke(this, (mouseX, mouseY));
                         }
-                        
+
                         break;
                     }
 
