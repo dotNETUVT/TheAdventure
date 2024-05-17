bf03c893ddfc946a1296e3c70cda48f284271e34
Álvaro Freire Ares <alvaro.freire10@e-uvt.ro>
2024-05-17 16:19:03 +0300

    format code - dotnet format

--- Models/Data/Level.cs
+++ Models/Data/Level.cs
@@ -1,8 +1,9 @@
-public class Level{
+public class Level
+{
     public int Width { get; set; }
-    public int Height{ get; set; }
+    public int Height { get; set; }
     public TileSetReference[] TileSets { get; set; } = Array.Empty<TileSetReference>();
     public Layer[] Layers { get; set; } = Array.Empty<Layer>();
-    public int TileWidth {get;set;}
-    public int TileHeight {get;set;}
+    public int TileWidth { get; set; }
+    public int TileHeight { get; set; }
 }
--- Models/Data/Utils.cs
+++ Models/Data/Utils.cs
@@ -1,11 +1,13 @@
 namespace TheAdventure.Models.Data;
 
-public struct FramePosition{
+public struct FramePosition
+{
     public int Row { get; set; }
     public int Col { get; set; }
 }
 
-public struct FrameOffset{
+public struct FrameOffset
+{
     public int OffsetX { get; set; }
     public int OffsetY { get; set; }
 }
