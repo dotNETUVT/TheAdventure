c31d869a1daa8be4b176da6f3ac1d90184ea5e1c
RazvanBatrinuUVT <razvan.batrinu03@e-uvt.ro>
2024-05-18 16:34:30 +0300

    Update TemporaryGameObject.cs

--- Models/TemporaryGameObject.cs
+++ Models/TemporaryGameObject.cs
@@ -1,18 +1,23 @@
 using Silk.NET.SDL;
+using System;
 
-namespace TheAdventure.Models;
-
-public class TemporaryGameObject : RenderableGameObject
+namespace TheAdventure.Models
 {
-    public double Ttl { get; init; }
-    public bool IsExpired => (DateTimeOffset.Now - _spawnTime).TotalSeconds >= Ttl;
-    
-    private DateTimeOffset _spawnTime;
-    
-    public TemporaryGameObject(SpriteSheet spriteSheet, double ttl, (int X, int Y) position, double angle = 0.0, Point rotationCenter = new())
-        : base(spriteSheet, position, angle, rotationCenter)
+    public class TemporaryGameObject : RenderableGameObject
     {
-        Ttl = ttl;
-        _spawnTime = DateTimeOffset.Now;
+        public double Ttl { get; init; }
+        public bool IsExpired => (DateTimeOffset.Now - _spawnTime).TotalSeconds >= Ttl;
+
+        private DateTimeOffset _spawnTime;
+        public Action<TemporaryGameObject> OnExpire { get; set; }
+
+        public TemporaryGameObject(SpriteSheet spriteSheet, double ttl, (int X, int Y) position, Action<TemporaryGameObject> onExpire, double angle = 0.0, Point rotationCenter = new())
+            : base(spriteSheet, position, angle, rotationCenter)
+        {
+            Ttl = ttl;
+            _spawnTime = DateTimeOffset.Now;
+            OnExpire = onExpire;
+        }
+
     }
 }
