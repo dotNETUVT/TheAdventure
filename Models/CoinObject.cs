7c2e42a4322619a7f32d9fa91830a05ec5faba3c
RazvanBatrinuUVT <razvan.batrinu03@e-uvt.ro>
2024-05-19 14:37:17 +0300

    Added new objects

---
+++ Models/CoinObject.cs
@@ -0,0 +1,12 @@
+﻿using Silk.NET.SDL;
+
+namespace TheAdventure.Models
+{
+    public class CoinObject : RenderableGameObject
+    {
+        public CoinObject(SpriteSheet spriteSheet, (int X, int Y) position)
+            : base(spriteSheet, position)
+        {
+        }
+    }
+}
---
+++ Models/ObstacleObject.cs
@@ -0,0 +1,22 @@
+﻿using Silk.NET.SDL;
+
+namespace TheAdventure.Models
+{
+    public enum ObstacleType
+    {
+        Red,
+        Blue,
+        Yellow
+    }
+
+    public class ObstacleObject : RenderableGameObject
+    {
+        public ObstacleType ObstacleType { get; }
+
+        public ObstacleObject(SpriteSheet spriteSheet, (int X, int Y) position, ObstacleType obstacleType)
+            : base(spriteSheet, position)
+        {
+            ObstacleType = obstacleType;
+        }
+    }
+}
