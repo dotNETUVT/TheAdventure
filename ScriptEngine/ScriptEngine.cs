025895e1b09b7bcbb193599146df46344cd5cbbd
Gongu Ilie-Cristian <ilie.gongu01@e-uvt.ro>
2024-05-20 20:08:36 +0300

    Improving Resource Management, Error Handling, Code Organization and Readability

--- ScriptEngine/ScriptEngine.cs
+++ ScriptEngine/ScriptEngine.cs
@@ -1,134 +1,175 @@
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
-namespace TheAdventure;
-
-public class ScriptEngine
+namespace TheAdventure
 {
-    private PortableExecutableReference[] scriptReferences;
-
-    private Dictionary<string, IScript> _scripts = new Dictionary<string, IScript>();
-    private FileSystemWatcher _watcher;
-
-    public ScriptEngine()
+    public class ScriptEngine : IDisposable
     {
-        var rtPath = Path.GetDirectoryName(typeof(object).Assembly.Location) +
-                 Path.DirectorySeparatorChar;
+        private readonly PortableExecutableReference[] _scriptReferences;
+        private readonly ConcurrentDictionary<string, IScript> _scripts = new ConcurrentDictionary<string, IScript>();
+        private FileSystemWatcher? _watcher;
+        private bool _disposed;
 
-        var references = new string[] {
-            #region .Net SDK
-            rtPath + "System.Private.CoreLib.dll",
-            rtPath + "System.Runtime.dll",
-            rtPath + "System.Console.dll",
-            rtPath + "netstandard.dll",
+        public ScriptEngine()
+        {
+            var rtPath = Path.GetDirectoryName(typeof(object).Assembly.Location) +
+                         Path.DirectorySeparatorChar;
 
-            rtPath + "System.Text.RegularExpressions.dll", // IMPORTANT!
-            rtPath + "System.Linq.dll",
-            rtPath + "System.Linq.Expressions.dll", // IMPORTANT!
-
-            rtPath + "System.IO.dll",
-            rtPath + "System.Net.Primitives.dll",
-            rtPath + "System.Net.Http.dll",
-            rtPath + "System.Private.Uri.dll",
-            rtPath + "System.Reflection.dll",
-            rtPath + "System.ComponentModel.Primitives.dll",
-            rtPath + "System.Globalization.dll",
-            rtPath + "System.Collections.Concurrent.dll",
-            rtPath + "System.Collections.NonGeneric.dll",
-            rtPath + "Microsoft.CSharp.dll",
-            #endregion
-            typeof(IScript).Assembly.Location
-        };
-        scriptReferences = references.Select(x => MetadataReference.CreateFromFile(x)).ToArray();
-    }
-
-    public void AttachWatcher(string path){
-        _watcher = new FileSystemWatcher(path);
-        _watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.LastAccess | NotifyFilters.Size;
-        _watcher.Changed += OnScriptChanged;
-        _watcher.Deleted += OnScriptChanged;
-        _watcher.EnableRaisingEvents = true;
-    }
-
-    public void OnScriptChanged(object source, FileSystemEventArgs e){
-        if(_scripts.ContainsKey(e.FullPath)){
-            Console.WriteLine($"Change detected for: {e.FullPath}");
-            switch(e.ChangeType)
+            var references = new[]
             {
-                case WatcherChangeTypes.Changed:
-                    RemoveScript(e.FullPath);
-                    Load(e.FullPath);
-                    break;
-                case WatcherChangeTypes.Deleted:
-                    RemoveScript(e.FullPath);
-                    break;
+                #region .Net SDK
+                rtPath + "System.Private.CoreLib.dll",
+                rtPath + "System.Runtime.dll",
+                rtPath + "System.Console.dll",
+                rtPath + "netstandard.dll",
+                rtPath + "System.Text.RegularExpressions.dll", // IMPORTANT!
+                rtPath + "System.Linq.dll",
+                rtPath + "System.Linq.Expressions.dll", // IMPORTANT!
+                rtPath + "System.IO.dll",
+                rtPath + "System.Net.Primitives.dll",
+                rtPath + "System.Net.Http.dll",
+                rtPath + "System.Private.Uri.dll",
+                rtPath + "System.Reflection.dll",
+                rtPath + "System.ComponentModel.Primitives.dll",
+                rtPath + "System.Globalization.dll",
+                rtPath + "System.Collections.Concurrent.dll",
+                rtPath + "System.Collections.NonGeneric.dll",
+                rtPath + "Microsoft.CSharp.dll",
+                #endregion
+                typeof(IScript).Assembly.Location
+            };
+            _scriptReferences = references.Select(x => MetadataReference.CreateFromFile(x)).ToArray();
+        }
+
+        public void AttachWatcher(string path)
+        {
+            if (_watcher != null)
+            {
+                _watcher.Dispose();
+            }
+
+            _watcher = new FileSystemWatcher(path)
+            {
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.LastAccess | NotifyFilters.Size,
+                EnableRaisingEvents = true
+            };
+            _watcher.Changed += OnScriptChanged;
+            _watcher.Deleted += OnScriptChanged;
+        }
+
+        private void OnScriptChanged(object source, FileSystemEventArgs e)
+        {
+            if (_scripts.ContainsKey(e.FullPath))
+            {
+                Console.WriteLine($"Change detected for: {e.FullPath}");
+                switch (e.ChangeType)
+                {
+                    case WatcherChangeTypes.Changed:
+                        RemoveScript(e.FullPath);
+                        Load(e.FullPath);
+                        break;
+                    case WatcherChangeTypes.Deleted:
+                        RemoveScript(e.FullPath);
+                        break;
+                }
             }
         }
-    }
 
-    public void RemoveScript(string path){
-        if(_scripts.TryGetValue(path, out var script)){
-            _scripts.Remove(path);
+        public void RemoveScript(string path)
+        {
+            _scripts.TryRemove(path, out _);
         }
-    }
 
-    public void LoadAll(string scriptFolder){
-        AttachWatcher(scriptFolder);
-        var dirInfo = new DirectoryInfo(scriptFolder);
-        if(dirInfo.Exists){
-            foreach(var file in dirInfo.GetFiles()){
-                if (file.Extension.ToLowerInvariant() == ".script"){
+        public void LoadAll(string scriptFolder)
+        {
+            AttachWatcher(scriptFolder);
+            var dirInfo = new DirectoryInfo(scriptFolder);
+            if (dirInfo.Exists)
+            {
+                foreach (var file in dirInfo.GetFiles("*.script"))
+                {
                     try
                     {
                         Load(file.FullName);
                     }
-                    catch(Exception ex){
+                    catch (Exception ex)
+                    {
                         Console.WriteLine($"Exception trying to load {file.FullName}");
                         Console.WriteLine(ex);
                     }
                 }
             }
         }
-    }
 
-    public void ExecuteAll(Engine engine){
-        foreach(var script in _scripts){
-            script.Value.Execute(engine);
+        public void ExecuteAll(Engine engine)
+        {
+            foreach (var script in _scripts.Values)
+            {
+                script.Execute(engine);
+            }
         }
-    }
 
-    public IScript? Load(string file)
-    {
-        Console.WriteLine($"Loading script {file}");
-        FileInfo fileInfo = new FileInfo(file);
-        var fileOutput = fileInfo.FullName.Replace(fileInfo.Extension, ".dll");
-        var code = File.ReadAllText(fileInfo.FullName);
-        var syntaxTree = CSharpSyntaxTree.ParseText(code);
-        var compilation = CSharpCompilation.Create(fileInfo.Name.Replace(fileInfo.Extension, string.Empty), new [] { syntaxTree }, 
-           scriptReferences, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-        using (var compiledScriptAssembly = new FileStream(fileOutput, FileMode.OpenOrCreate)){
-            var result = compilation.Emit(compiledScriptAssembly);
-            if(!result.Success){
-                foreach(var diag in result.Diagnostics){
-                    if(diag.Severity == DiagnosticSeverity.Error ){
-                        Console.WriteLine(string.Join(";", diag.Descriptor.CustomTags));
-                        Console.WriteLine($"{diag.Descriptor.MessageFormat.ToString()} - {code.Substring(diag.Location.SourceSpan.Start, diag.Location.SourceSpan.Length)} - {diag.Descriptor.HelpLinkUri.ToString()} - {diag.Location.ToString()}");
+        public IScript? Load(string file)
+        {
+            Console.WriteLine($"Loading script {file}");
+            var fileInfo = new FileInfo(file);
+            var fileOutput = fileInfo.FullName.Replace(fileInfo.Extension, ".dll");
+            var code = File.ReadAllText(fileInfo.FullName);
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+            var compilation = CSharpCompilation.Create(fileInfo.Name.Replace(fileInfo.Extension, string.Empty),
+                new[] { syntaxTree },
+                _scriptReferences,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var compiledScriptAssembly = new FileStream(fileOutput, FileMode.OpenOrCreate))
+            {
+                var result = compilation.Emit(compiledScriptAssembly);
+                if (!result.Success)
+                {
+                    foreach (var diag in result.Diagnostics.Where(diag => diag.Severity == DiagnosticSeverity.Error))
+                    {
+                        Console.WriteLine($"{diag.GetMessage()} at {diag.Location}");
                     }
+                    throw new FileLoadException($"Failed to load script: {file}");
                 }
-                throw new FileLoadException(file);
+            }
+
+            var assembly = Assembly.LoadFile(fileOutput);
+            foreach (var type in assembly.GetTypes().Where(type => typeof(IScript).IsAssignableFrom(type)))
+            {
+                if (Activator.CreateInstance(type) is IScript instance)
+                {
+                    instance.Initialize();
+                    _scripts[file] = instance;
+                    return instance;
+                }
+            }
+
+            return null;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _watcher?.Dispose();
+                }
+
+                _disposed = true;
             }
         }
-        foreach(var type in Assembly.LoadFile(fileOutput).GetTypes()){
-            if (type.IsAssignableTo(typeof(IScript))){
-                var instance = (IScript)type.GetConstructor(Type.EmptyTypes)?.Invoke(null);
-                instance.Initialize();
-                if(instance != null){
-                    _scripts.Add(file, instance);
-                }
-                return instance;
-            }
-        }
-        return null;
     }
 }
